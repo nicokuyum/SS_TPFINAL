8cc59fb0e3335358e98a6818a31f74922b5f3c33
nicokuyum <nicokuyum@hotmail.com>
2016-07-15 15:16:36 -0300

    Clases Vector

---
+++ src/ClothModel/Vector2D.java
@@ -0,0 +1,9 @@
+package ClothModel;
+
+public class Vector2D extends Vector3D{
+
+	public Vector2D(double x, double y) {
+		super(x,y,0);
+	}
+
+}
---
+++ src/ClothModel/Vector3D.java
@@ -0,0 +1,53 @@
+package ClothModel;
+
+public class Vector3D {
+	
+	private double x,y,z;
+	
+	public Vector3D(double x, double y, double z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public double getDistance(Vector3D other){
+		return Math.sqrt(Math.pow(this.x, other.x) + Math.pow(this.y, other.y) + Math.pow(this.z, other.z));
+	}
+	
+	public void multiply(double value){
+		this.x *= value;
+		this.y *= value;
+		this.z *= value;
+	}
+	
+	public void divide(double value){
+		this.x /= value;
+		this.y /= value;
+		this.z /= value;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public double getZ() {
+		return z;
+	}
+	
+	public void setX(double x) {
+		this.x = x;
+	}
+	
+	public void setY(double y) {
+		this.y = y;
+	}
+	
+	public void setZ(double z) {
+		this.z = z;
+	}
+
+}
